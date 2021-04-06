a6f3f8ffff0e3ec1fe6f043408853a3b80bf0ae9
Chrystian <chrystianrz@hotmail.com>
2021-04-06 12:34:11 -0300

    feat: added macro and time_points list

--- app/views/timeline_view.py
+++ app/views/timeline_view.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from data.posts import time_points
 
 
 bp_timeline = Blueprint('bp_timeline', __name__)
 
+
 @bp_timeline.route('/pote3q')
 def timeline():
-    return render_template('index.html')
+    return render_template('timeline.html', time_points=time_points)
---
+++ data/posts.py
@@ -0,0 +1,46 @@
+time_points = [
+    {
+        'day_month': '12/04',
+        'day_number': 'Day 1',
+        'prop_title': 'Planejamento',
+        'description': """
+        O primeiro dia deve ser o de brainstorming de ideias,
+         visando encontrar um problema e mapear uma solução MVP para o mesmo.
+         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
+         ferramentas que utilizarão (Kanban, Design, etc)
+        """
+    },
+    {
+        'day_month': '13/04',
+        'day_number': 'Day 2',
+        'prop_title': 'Planejamento 2',
+        'description': """
+        O primeiro dia deve ser o de brainstorming de ideias,
+         visando encontrar um problema e mapear uma solução MVP para o mesmo.
+         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
+         ferramentas que utilizarão (Kanban, Design, etc)
+        """
+    },
+    {
+        'day_month': '14/04',
+        'day_number': 'Day 3',
+        'prop_title': 'Planejamento 3',
+        'description': """
+        O primeiro dia deve ser o de brainstorming de ideias,
+         visando encontrar um problema e mapear uma solução MVP para o mesmo.
+         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
+         ferramentas que utilizarão (Kanban, Design, etc)
+        """
+    },
+    {
+        'day_month': '15/04',
+        'day_number': 'Day 4',
+        'prop_title': 'Planejamento 4',
+        'description': """
+        O primeiro dia deve ser o de brainstorming de ideias,
+         visando encontrar um problema e mapear uma solução MVP para o mesmo.
+         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
+         ferramentas que utilizarão (Kanban, Design, etc)
+        """
+    },
+]
