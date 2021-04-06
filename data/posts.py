c5ad56b61fa0699b32e046f6011853534f8043a6
Chrystian <chrystianrz@hotmail.com>
2021-04-06 14:12:08 -0300

    feat: added all 10 days of projects

--- data/posts.py
+++ data/posts.py
@@ -4,43 +4,117 @@ time_points = [
         'day_number': 'Day 1',
         'prop_title': 'Planejamento',
         'description': """
-        O primeiro dia deve ser o de brainstorming de ideias,
-         visando encontrar um problema e mapear uma solução MVP para o mesmo.
-         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
-         ferramentas que utilizarão (Kanban, Design, etc)
+        Definido o projeto, o primeiro dia visa organizar as ideias e materializa-las
+         em alguma ferramenta de gerenciamento de tasks (Trello, Asana, etc). Além disso,
+         também são definidas as ferramentas e bibliotecas que serão utilizadas, assim
+         como o padrão que o projeto seguirá (gitflow), quais os endpoints necessários, 
+         qual a forma de persistência no banco de dados, etc...
         """
     },
     {
         'day_month': '13/04',
         'day_number': 'Day 2',
-        'prop_title': 'Planejamento 2',
+        'prop_title': 'Sazonamento',
         'description': """
-        O primeiro dia deve ser o de brainstorming de ideias,
-         visando encontrar um problema e mapear uma solução MVP para o mesmo.
-         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
-         ferramentas que utilizarão (Kanban, Design, etc)
+        Dado o tempo de maturação das ideias discutidas no dia 1, o dia 2 segue
+         amadurecendo a discussão para trazer novos problemas e visões sobre o
+         projeto. Ao fim do dia, deve-se ter um documento descrevendo sobre o que
+         o projeto vai ser, quais bibliotecas e tecnologias serão usadas, qual a
+         motivação por trás da ideia do projeto e o que se tem planejado como MVP.
         """
     },
     {
         'day_month': '14/04',
         'day_number': 'Day 3',
-        'prop_title': 'Planejamento 3',
+        'prop_title': 'Execução das Tasks',
         'description': """
-        O primeiro dia deve ser o de brainstorming de ideias,
-         visando encontrar um problema e mapear uma solução MVP para o mesmo.
-         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
-         ferramentas que utilizarão (Kanban, Design, etc)
+        Inicio do dia 3 deve ser de organização básica sobre o dia na daily, divisão
+         de tasks caso nao tenham dividido ainda, e execução das mesmas. Começar a
+         modelagem do banco de dados é essencial para uma base sólida do projeto!
         """
     },
     {
         'day_month': '15/04',
         'day_number': 'Day 4',
-        'prop_title': 'Planejamento 4',
+        'prop_title': 'Execução das Tasks 2',
         'description': """
-        O primeiro dia deve ser o de brainstorming de ideias,
-         visando encontrar um problema e mapear uma solução MVP para o mesmo.
-         O grupo deve: Definir a solução, quais libs irão utilizar, e escolher as
-         ferramentas que utilizarão (Kanban, Design, etc)
+        Inicio do dia 4 deve ser sobre o que cada um conseguiu desempenhar no dia
+         anterior em relação as tasks, além da continuidade ou redistribuição de novas
+         tarefas. Durante a execução, é comum novas ideias e melhorias surgirem,
+         não deixem de fomentar o backlog ou de se reunirem para discutir mudanças
+         pertinentes nos planos.
+        """
+    },
+    {
+        'day_month': '16/04',
+        'day_number': 'Day 5',
+        'prop_title': 'Retrospectiva 1',
+        'description': """
+        Inicio do dia 5 deve ser sobre organizar o que ja foi feito, organizar o
+         que falta para terminar as tasks e fazer um planejamento sobre qual será
+         o ritmo da semana seguinte. Ao fim do dia, façam uma retrospectiva elencando
+         os pontos bons, de melhoria e ruins do trabalho que desempenharam como time
+         até agora, documentando cada ponto para entregar para a equipe de ensino.
+        """
+    },
+    {
+        'day_month': '19/04',
+        'day_number': 'Day 6',
+        'prop_title': 'Planejamento 2',
+        'description': """
+        Inicio do dia 6 deve ser sobre ajustar alguns pontos que possam ter ficado
+         da semana anterior, tanto na organização quanto na execução. Reorganização
+         e divisão de novas tasks é o primordial, sempre alinhando a quantidade de
+         trabalho com o tempo restante de projeto. Aqui vocês já devem ter uma base
+         bem montada para dar continuidade a reta final do capstone.
+        """
+    },
+    {
+        'day_month': '20/04',
+        'day_number': 'Day 7',
+        'prop_title': 'Projeção de MVP',
+        'description': """
+        Estruturada a semana 2, neste dia deve-se executar as tasks divididas no
+         planejamento, sempre atento a quantidade de tempo restante. Caso ainda 
+         estejam trabalhando em módulos separados, verificar a integridade do projeto
+         completo é necessário para nao ter bugs surpresa quando mergearem as diferentes
+         branchs !
+        """
+    },
+    {
+        'day_month': '21/04',
+        'day_number': 'Day 8',
+        'prop_title': 'Ajustes finais',
+        'description': """
+        Etapa final do projeto, ele já deve estar estruturado e rodando com
+         as funcionalidades básicas descritas no documento MVP fornecido pela equipe
+         ao fim do dia. É importante que diferentes membros do time utilizem a aplicação
+         separadamente para caçar qualquer bug que tenha passado despercebido durante
+         os dias anteriores, a fim de polir o sistema para a entrega.
+        """
+    },
+    {
+        'day_month': '22/04',
+        'day_number': 'Day 9',
+        'prop_title': 'Retrospectiva 2',
+        'description': """
+        O projeto deve estar entregável, ajustes finais no código podem ser feitos.
+         Durante esse dia, a documentação README.md contendo os passos para a
+         utilização da api deve estar completa. Ao fim, deverão fazer uma restrospectiva
+         geral sobre o projeto e o trabalho em equipe, trazendo novamente os pontos
+         citados na retro 1 para refletir se estes foram melhorados, alem de elencar
+         novos pontos o uma visão geral sobre como foi trabalhar com a equipe.
+        """
+    },
+    {
+        'day_month': '23/04',
+        'day_number': 'Day 10',
+        'prop_title': 'Apresentação Capstone Q3',
+        'description': """
+        Dia de apresentação do projeto. Cada apresentação deve ter cerca de 20 minutos,
+         e deve contemplar a motivação da criação do sistema, tecnologias utilizadas,
+         funcionalidades do sistema, ambições sobre um futuro upgrade nas features e
+         visão geral de como se organizaram para a execução do projeto.
         """
     },
 ]
